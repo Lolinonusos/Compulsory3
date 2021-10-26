61e696242152d01cf6409b8a1c022c58fadf2055
Linus Nordbakken Nagy <linus.nordbakken@gmail.com>
2021-10-27 00:11:29 +0200

    We draw dat board

--- Compulsory3/Source.cpp
+++ Compulsory3/Source.cpp
@@ -2,8 +2,70 @@
 
 // Making connect 4 shiddass
 
-int board[7][6];
+// For the game
+char board[7][7] = { { ' ',' ',' ',' ',' ',' ',' ' },
+					 { '.','.','.','.','.','.','.' },
+					 { '.','.','.','.','.','.','.' },
+					 { '.','.','.','.','.','.','.' },
+					 { '.','.','.','.','.','.','.' },
+					 { '.','.','.','.','.','.','.' },
+					 { '.','.','.','.','.','.','.' } };
+
+char menu();
+void rules();
+void drawBoard();
+void gaming();
+
+// Universal stuff
+void clearCin();
 
 int main() {
+	//while (true) {
+		menu();
+
+
+		gaming();
+
+	//}
+	return 0;
+}
+
+char menu() {
+	rules();
+	return 4;
+}
+
+void rules() {
+
+}
+
+void drawBoard() {
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; i < 7; i++) {
+		for (j = 0; j < 7; j++) {
+			if (i == 0) {
+				std::cout << "T" << board[i][j];
+			}
+			else {
+				std::cout << "|" << board[i][j];
+			}
+		}
+		if (i == 0) {
+			std::cout << "T" << std::endl;
+		}
+		else {
+			std::cout << "|" << std::endl;
+		}
+	}
+	std::cout << "---------------" << std::endl;
+}
+
+void gaming() {
+	drawBoard();
+}
+
+void clearCin() {
 
 }
