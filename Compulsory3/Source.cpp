8567fd69de5f32364f49e0f510886d40c880aee9
Lolinonusos <linus.nordbakken@gmail.com>
2021-11-04 19:21:44 +0100

    I can finally move a thing through arrays and vectors o_o'

--- Compulsory3/Source.cpp
+++ Compulsory3/Source.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
-
+#include <conio.h>
+#include <vector>
 // Making connect 4 shiddass
 
 
 
 // For the game
-char board[7][7] = { { ' ',' ',' ',' ',' ',' ',' ' },
+std::vector<std::vector<char>> board;
+/* = { { ' ',' ',' ',' ',' ',' ',' ' },
 					 { '.','.','.','.','.','.','.' },
 					 { '.','.','.','.','.','.','.' },
 					 { '.','.','.','.','.','.','.' },
 					 { '.','.','.','.','.','.','.' },
 					 { '.','.','.','.','.','.','.' },
-					 { '.','.','.','.','.','.','.' } };
+					 { '.','.','.','.','.','.','.' } };*/
 
-char selectColumn[7];
+int arrowPosX = 0;
+int arrowPosY = 0;
+char selectColumn = 'v';
 
 void menu();
 void rules();
 void drawBoard();
 void gaming();
+void placeToken(char);
 void resetBoard();
 
 // Universal stuff
@@ -26,6 +31,18 @@ void clearCin();
 
 // Main wil take the role as a menu
 int main() {
+
+	// This is how you make a 2D vector
+	board.push_back(std::vector<char>(7, ' '));
+	board.push_back(std::vector<char>(7, ' '));
+	board.push_back(std::vector<char>(7, ' '));
+	board.push_back(std::vector<char>(7, ' '));
+	board.push_back(std::vector<char>(7, ' '));
+	board.push_back(std::vector<char>(7, ' '));
+	board.push_back(std::vector<char>(7, ' '));
+	board.push_back(std::vector<char>(7, ' '));
+
+
 	while (true) {
 		int menuChoice{};
 
@@ -37,7 +54,7 @@ int main() {
 		}
 
 		gaming();
-
+		break;
 	}
 	return 0;
 }
@@ -50,28 +67,75 @@ void drawBoard() {
 	int i = 0;
 	int j = 0;
 
-	for (i = 0; i < 7; i++) {
-		for (j = 0; j < 7; j++) {
-			if (i == 0) {
-				std::cout << "T" << board[i][j];
+	
+	for (i = 0; i < board.size(); i++) { // Checks the outer vector
+		for (j = 0; j < board.at(i).size(); j++) { // Checks the vector at position i
+			board.at(i).at(j) = '.'; // Draws periods troghout the vector
+			if (i == arrowPosX && j == arrowPosY) { // We draw the arrow pointer at the correct x y position
+				board[i][j] = selectColumn;
 			}
-			else {
-				std::cout << "|" << board[i][j];
-			}
+			/*else {
+				board[i][j] = '.';
+			}*/
+			std::cout << "|" << board.at(i).at(j);
 		}
-		if (i == 0) {
-			std::cout << "T" << std::endl;
-		}
-		else {
-			std::cout << "|" << std::endl;
-		}
+		std::cout << "|" << std::endl;
 	}
 	std::cout << "---------------" << std::endl;
 }
 
 void gaming() {
-	drawBoard();
+	char move{};
 
+	while (true) {
+		system("cls");
+		std::cout << "Use 'a' or 'd' to move pointer left or right respectively.\n";
+		std::cout << "Press enter to select the column you have highlighted.\n";
+		std::cout << "Or you can use number keys 1 through 7 to instantly select the column you want" << std::endl;
+		drawBoard();
+		move = _getch();
+		move = tolower(move);
+		switch (move) {
+			// First two cases 'a' and 'd' allows users to move pointers over different columns
+		case 'a':
+			arrowPosY--;
+			if (arrowPosY < 0) {
+				arrowPosY = board.size() - 1;
+			}
+			break;
+		case 'd':
+			arrowPosY++;
+			if (arrowPosY > (board.size() - 1)) {
+				arrowPosY = 0;
+			}
+			break;
+		case '1':
+			std::cout << "You selected:" << move << std::endl << std::endl;
+			break;
+		case '2':
+			std::cout << "You selected:" << move << std::endl << std::endl;
+			break;
+		case '3':
+			std::cout << "You selected:" << move << std::endl << std::endl;
+			break;
+		case '4':
+			std::cout << "You selected:" << move << std::endl << std::endl;
+			break;
+		case '5':
+			std::cout << "You selected:" << move << std::endl << std::endl;
+			break;
+		case '6':
+			std::cout << "You selected:" << move << std::endl << std::endl;
+			break;
+		case '7':
+			std::cout << "You selected:" << move << std::endl << std::endl;
+			break;
+		default:
+			std::cout << "Use one of the specified inputs please" << std::endl << std::endl;
+			break;
+		}
+
+	}
 }
 
 void clearCin() {
