5bdb71820007d2722a3462ede4b4a67dd007ab0c
Linus Nordbakken Nagy <linus.nordbakken@gmail.com>
2021-11-13 01:11:49 +0100

    Start on saving and RNGesus.

--- Compulsory3/Source.cpp
+++ Compulsory3/Source.cpp
@@ -12,7 +12,7 @@ public:
 	std::string name{};
 	int wins{};
 	int draws{};
-	int forfeit{};
+	int forfeits{};
 
 private:
 
@@ -24,7 +24,7 @@ std::vector <Player> players;
 // For the game
 std::vector<std::vector<char>> board;
 
-// Foldering
+// Foldering, currently not doing anything with the file :(
 std::string buffer{};
 std::streampos begin, end;
 std::fstream stats("../Names_And_Stats.txt", std::ios::in | std::ios::app | std::ios::out); // Se etter en bedre måte
@@ -46,6 +46,7 @@ std::string menu[4][2] = { { " ", " 1. Play"},
 
 // This collection is displayed on the menu in some form
 void whoIsGaming(); // Insert name of players, we will try to save them and their scores. Will go to gaming when finished
+void createPlayerName();
 void rules(); // Display the rules
 void viewStats(); // Display stats
 void quit(); // Terminate program 
@@ -61,6 +62,9 @@ bool win = false;
 bool p1Win = false;
 bool p2Win = false;
 void resetBoard(); // Empty the board of tokens
+
+// AI
+void RNGesus();
 
 // Universal stuff
 void clearCin();
@@ -151,6 +155,20 @@ int main() {
 }
 
 void whoIsGaming() {
+	if (players.size() != 0) {
+		char yn{};
+		std::cout << "Do you want to create new names?";
+		yn = _getch();
+		if (yn == 'y') {
+			createPlayerName();
+		}
+	}
+	else {
+		createPlayerName();
+	}
+}
+	
+void createPlayerName() {
 
 	std::string p1Name{};
 	std::string p2Name{};
@@ -162,8 +180,6 @@ void whoIsGaming() {
 	std::cout << std::endl;
 	std::cout << "Player 2: ";
 	std::cin >> p2Name;
-
-	
 	stats.close();
 }
 
@@ -225,7 +241,21 @@ void gaming() {
 	turn = 1; // P1 always starts
 	system("cls");
 	while (win != true) {
-		
+		system("cls");
+
+		// Spelling out whose turn it is
+		switch (turn) {
+		case 1:
+			std::cout << " It is " << std::endl;
+			break;
+		}
+
+		// We will fund out how to give RNGesus life on a later time...
+		//if (Player.name == "AI" || Player.name == "Ai" || Player.name == "ai" || Player.name == "RNGesus") {
+		//	RNGesus();
+		//}
+
+
 		std::cout << "Use 'a' or 'd' to move the pointer; 'v' left or right respectively.\n";
 		std::cout << "Then you can press ENTER or SPACE to select the highlighted column.\n";
 		std::cout << "Or you can use number keys 1 through 7 to instantly select the column you want" << std::endl;
@@ -297,8 +327,8 @@ void gaming() {
 			break;
 		}
 		std::cout << arrowPosY << "\t" << arrowPosX << std::endl; // Fjærn når du er ferdig
-		}
 	}
+}
 
 
 // Will move the token to the bottom of a column
@@ -359,8 +389,14 @@ void winConditions() {
 		for (int y = 0; y < board.size() - 3; y++) { // Checks the outer vector, 
 			for (int x = 0; x < board.at(y).size(); x++) { // Checks the vector at position i
 				if (board[y][x] != '.') {
-					if (board[y][x] == board[y + 1][x] && board[y + 1][x] == board[y + 2][x] && board[y + 2][x] == board[y + 3][x]) {
-						std::cout << "Vertical win" << std::endl;
+					if (board[y][x] == 'X' && board[y][x] == board[y + 1][x] && board[y + 1][x] == board[y + 2][x] && board[y + 2][x] == board[y + 3][x]) {
+						std::cout << "P1 Vertical win" << std::endl;
+						win = true;
+						break;
+					}
+					if (board[y][x] == 'O' && board[y][x] == board[y + 1][x] && board[y + 1][x] == board[y + 2][x] && board[y + 2][x] == board[y + 3][x]) {
+						std::cout << "P2 Vertical win" << std::endl;
+						win = true;
 						break;
 					}
 				}
@@ -370,8 +406,14 @@ void winConditions() {
 		for (int y = 1; y < board.size(); y++) {
 			for (int x = 0; x < board.at(y).size() - 3; x++) {
 				if (board[y][x] != '.') { // Endre til X eller O for spiller spesifik seier
-					if (board[y][x] == board[y][x + 1] && board[y][x + 1] == board[y][x + 2] && board[y][x + 2] == board[y][x + 3]) {
-						std::cout << "Horizontal win" << std::endl;
+					if (board[y][x] == 'X' && board[y][x] == board[y][x + 1] && board[y][x + 1] == board[y][x + 2] && board[y][x + 2] == board[y][x + 3]) {
+						std::cout << "P1 Horizontal win" << std::endl;
+						win = true;
+						break;
+					}
+					if (board[y][x] == 'O' && board[y][x] == board[y][x + 1] && board[y][x + 1] == board[y][x + 2] && board[y][x + 2] == board[y][x + 3]) {
+						std::cout << "P2 Vertical win" << std::endl;
+						win = true;
 						break;
 					}
 				}
@@ -382,8 +424,14 @@ void winConditions() {
 		for (int y = 1; y < board.size() - 3; y++) {
 			for (int x = 0; x < board.at(y).size() - 3; x++) {
 				if (board[y][x] != '.') {
-					if (board[y][x] == board[y+1][x + 1] && board[y +1][x + 1] == board[y + 2][x + 2] && board[y + 2][x + 2] == board[y + 3][x + 3]) {
-						std::cout << "Diagonal leaning left win" << std::endl;
+					if (board[y][x] == 'X' && board[y][x] == board[y+1][x + 1] && board[y +1][x + 1] == board[y + 2][x + 2] && board[y + 2][x + 2] == board[y + 3][x + 3]) {
+						std::cout << "P1 Diagonal leaning left win" << std::endl;
+						win = true;
+						break;
+					}
+					if (board[y][x] == 'O' && board[y][x] == board[y + 1][x + 1] && board[y + 1][x + 1] == board[y + 2][x + 2] && board[y + 2][x + 2] == board[y + 3][x + 3]) {
+						std::cout << "P2 Vertical win" << std::endl;
+						win = true;
 						break;
 					}
 				}
@@ -392,11 +440,16 @@ void winConditions() {
 
 	// Check diagonal leaning left
 		for (int y = 1; y < board.size() - 3; y++) {
-			for (int x = 6; x > board.at(y).size() -3; x--) { // Run in reverse to avoid 
+			for (int x = 6; x > board.at(y).size() -3; x--) { // Run in reverse to avoid error
 				if (board[y][x] != '.') {
-					if (board[y][x] == board[y + 1][x - 1] && board[y + 1][x - 1] == board[y + 2][x - 2] && board[y + 2][x - 2] == board[y + 3][x - 3]) {
-						std::cout << "Diagonal leaning right win" << std::endl;
-
+					if (board[y][x] == 'X' && board[y][x] == board[y + 1][x - 1] && board[y + 1][x - 1] == board[y + 2][x - 2] && board[y + 2][x - 2] == board[y + 3][x - 3]) {
+						std::cout << "P1 Diagonal leaning right win" << std::endl;
+						win = true;
+						break;
+					}
+					if (board[y][x] == 'O' && board[y][x] == board[y + 1][x - 1] && board[y + 1][x - 1] == board[y + 2][x - 2] && board[y + 2][x - 2] == board[y + 3][x - 3]) {
+						std::cout << "P2 Vertical win" << std::endl;
+						win = true;
 						break;
 					}
 				}
